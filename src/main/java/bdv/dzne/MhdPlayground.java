cf0cc8fb7316ea9ee58a70db085dae201df34cc3
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-10-07 15:16:33 +0200

    basically working

--- src/main/java/bdv/dzne/MhdPlayground.java
+++ src/main/java/bdv/dzne/MhdPlayground.java
@@ -5,17 +5,34 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Actions;
+
+import bdv.tools.transformation.TransformedSource;
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.util.BdvStackSource;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.FileInfo;
 import ij.plugin.PlugIn;
 import net.imagej.patcher.LegacyEnvironment;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.ARGBType;
 
 public class MhdPlayground implements PlugIn
 {
 	public static void main( final String[] args ) throws ClassNotFoundException
 	{
+		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+
 		System.setProperty( "ij.dir", "/Users/Pietzsch/Desktop/Fiji.app" );
 		System.setProperty( "fiji.dir", "/Users/Pietzsch/Desktop/Fiji.app" );
 		System.setProperty( "imagej.dir", "/Users/Pietzsch/Desktop/Fiji.app" );
 		System.setProperty( "ij1.plugin.dirs", "/Users/Pietzsch/Desktop/Fiji.app/plugins" );
+
 		final LegacyEnvironment ij1 = new LegacyEnvironment( null, false );
 		ij1.addPluginClasspath( Thread.currentThread().getContextClassLoader() );
 		ij1.main();
@@ -36,19 +53,94 @@ public class MhdPlayground implements PlugIn
 		}
 	}
 
+	class ImpInfo
+	{
+		ImagePlus imp;
+
+		PartiallyParsedMhd header;
+
+		final AffineTransform3D sourceTransform = new AffineTransform3D();
+
+		BdvStackSource< ? > bdvSource;
+
+		ImpInfo( final ImagePlus imp )
+		{
+			this.imp = imp;
+		}
+
+		void loadHeader() throws IOException
+		{
+			final FileInfo info = imp.getOriginalFileInfo();
+			String impfn = info.directory + "/" + info.fileName;
+			if ( impfn.endsWith( ".raw" ) )
+				impfn = impfn.substring( 0, impfn.length() - 4 ) + ".mhd";
+			final BufferedReader in = new BufferedReader( new InputStreamReader( new FileInputStream( impfn ) ) );
+			header = new PartiallyParsedMhd( in );
+			in.close();
+		}
+
+		void showInBdv( final Bdv bdv )
+		{
+			final FileInfo info = imp.getOriginalFileInfo();
+			final Img< ? > img = ImageJFunctions.wrap( imp );
+			header.getSourceTransform( sourceTransform );
+			bdvSource = BdvFunctions.show( img, info.fileName, Bdv.options().sourceTransform( sourceTransform ).addTo( bdv ) );
+		}
+	}
+
+	private Bdv bdv;
+
+	private ImpInfo atlas;
+
+	private ImpInfo input;
+
+	private void saveManualRegistration()
+	{
+		System.out.println( "save manual registration" );
+
+		final AffineTransform3D transform = getManualTransform( input.bdvSource ).preConcatenate( getManualTransform( atlas.bdvSource ).inverse() );
+		input.header.preConcatenateTransform( transform );
+		input.header.printModified();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+	private AffineTransform3D getManualTransform( final BdvStackSource< ? > stackSource )
+	{
+		final TransformedSource< ? > source = ( TransformedSource< ? > ) stackSource.getSources().get( 0 ).getSpimSource();
+		final AffineTransform3D manual = new AffineTransform3D();
+		final AffineTransform3D fixedTransform = new AffineTransform3D();
+		source.getIncrementalTransform( manual );
+		source.getFixedTransform( fixedTransform );
+		manual.concatenate( fixedTransform );
+		return manual;
+	}
+
 	public void doit() throws IOException
 	{
-		final String fn = "/Users/Pietzsch/Desktop/data/Christopher/Christopher Atlas/canon_T1_r(2).mha";
-//		final String fn = "/Users/Pietzsch/Desktop/data/Christopher/C2-Fused-1_turned_mirrored.mhd";
+//		final String fnAtlas = "/Users/Pietzsch/Desktop/data/Christopher/Christopher Atlas/canon_T1_r(2).mha";
+//		final String fnInput = "/Users/Pietzsch/Desktop/data/Christopher/C2-Fused-1_turned_mirrored.mhd";
+		final String fnAtlas = "/Users/Pietzsch/Desktop/data/Christopher/Results_prototype/atlas.mhd";
+		final String fnInput = "/Users/Pietzsch/Desktop/data/Christopher/Results_prototype/C2-95A.mhd";
 
-		final BufferedReader in = new BufferedReader( new InputStreamReader( new FileInputStream( fn ) ) );
-		final PartiallyParsedMhd header = new PartiallyParsedMhd( in );
-		in.close();
+		IJ.open( fnAtlas );
+		atlas = new ImpInfo( IJ.getImage() );
+		atlas.loadHeader();
+		atlas.showInBdv( null );
+		atlas.bdvSource.setColor( new ARGBType( 0x0000ff00 ) );
+		bdv = atlas.bdvSource;
 
-//		IJ.open( fn );
-//		final ImagePlus imp = new ImagePlus( fn );
-//		imp.show();
-//		final FileInfo info = imp.getOriginalFileInfo();
-//		final String fn2 = info.directory + "/" + info.fileName;
+		IJ.open( fnInput );
+		input = new ImpInfo( IJ.getImage() );
+		input.loadHeader();
+		input.showInBdv( bdv );
+		input.bdvSource.setColor( new ARGBType( 0x00ff00ff ) );
+		input.bdvSource.setCurrent();
+
+		final Actions actions = new Actions( new InputTriggerConfig() );
+		actions.install( bdv.getBdvHandle().getKeybindings(), "dzne" );
+		actions.runnableAction( this::saveManualRegistration, "save manual registration", "meta S" );
 	}
 }
--- src/main/java/bdv/dzne/PartiallyParsedMhd.java
+++ src/main/java/bdv/dzne/PartiallyParsedMhd.java
@@ -2,7 +2,10 @@ package bdv.dzne;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+
+import net.imglib2.realtransform.AffineTransform3D;
 
 public class PartiallyParsedMhd
 {
@@ -59,7 +62,7 @@ public class PartiallyParsedMhd
 					throw new IllegalArgumentException( "input .mhd file must have NDims = 3" );
 				found_NDims = true;
 			}
-			if ( line.startsWith( "TransformMatrix" ) )
+			else if ( line.startsWith( "TransformMatrix" ) )
 			{
 				parseVector( line, in, transformMatrix );
 				found_TransformMatrix = true;
@@ -111,6 +114,102 @@ public class PartiallyParsedMhd
 		catch ( final NumberFormatException e )
 		{
 			throw new ParseException( "Failed to parse vector of numbers.", e );
+		}
+	}
+
+	/**
+	 *
+	 * @param sourceTransform
+	 *            is set to the source-to-global transform, that transforms
+	 *            source coordinates into the global coordinates
+	 */
+	public void getSourceTransform( final AffineTransform3D sourceTransform )
+	{
+		final AffineTransform3D calibration = new AffineTransform3D();
+		calibration.set(
+				elementSpacing[ 0 ], 0, 0, 0,
+				0, elementSpacing[ 1 ], 0, 0,
+				0, 0, elementSpacing[ 2 ], 0 );
+
+		final AffineTransform3D calibratedToWorld = new AffineTransform3D();
+		calibratedToWorld.set(
+				transformMatrix[ 0 ], transformMatrix[ 1 ], transformMatrix[ 2 ], offset[ 0 ],
+				transformMatrix[ 3 ], transformMatrix[ 4 ], transformMatrix[ 5 ], offset[ 1 ],
+				transformMatrix[ 6 ], transformMatrix[ 7 ], transformMatrix[ 8 ], offset[ 2 ] );
+
+		sourceTransform.set( calibration );
+		sourceTransform.preConcatenate( calibratedToWorld );
+	}
+
+	public void preConcatenateTransform( final AffineTransform3D transform )
+	{
+		final AffineTransform3D calibratedToWorld = new AffineTransform3D();
+		calibratedToWorld.set(
+				transformMatrix[ 0 ], transformMatrix[ 1 ], transformMatrix[ 2 ], offset[ 0 ],
+				transformMatrix[ 3 ], transformMatrix[ 4 ], transformMatrix[ 5 ], offset[ 1 ],
+				transformMatrix[ 6 ], transformMatrix[ 7 ], transformMatrix[ 8 ], offset[ 2 ] );
+		calibratedToWorld.preConcatenate( transform );
+		for ( int r = 0; r < 3; ++r )
+		{
+			for ( int c = 0; c < 3; ++c )
+				transformMatrix[ 3 * r + c ] = calibratedToWorld.get( r, c );
+			offset[ r ] = calibratedToWorld.get( r, 3 );
+		}
+	}
+
+	public void printModified()
+	{
+		final PrintStream out = System.out;
+		for ( final String line : lines )
+		{
+			if ( line.startsWith( "TransformMatrix" ) )
+			{
+				final StringBuffer sb = new StringBuffer( "TransformMatrix = " );
+				for ( int i = 0; i < 9; ++i )
+				{
+					sb.append( transformMatrix[ i ] );
+					if ( i < 8 )
+						sb.append( " " );
+				}
+				out.println( sb );
+			}
+			else if ( line.startsWith( "Offset" ) )
+			{
+				final StringBuffer sb = new StringBuffer( "Offset = " );
+				for ( int i = 0; i < 3; ++i )
+				{
+					sb.append( offset[ i ] );
+					if ( i < 2 )
+						sb.append( " " );
+				}
+				out.println( sb );
+			}
+			else if ( line.startsWith( "CenterOfRotation" ) )
+			{
+				final StringBuffer sb = new StringBuffer( "CenterOfRotation = " );
+				for ( int i = 0; i < 3; ++i )
+				{
+					sb.append( centerOfRotation[ i ] );
+					if ( i < 2 )
+						sb.append( " " );
+				}
+				out.println( sb );
+			}
+			else if ( line.startsWith( "ElementSpacing" ) )
+			{
+				final StringBuffer sb = new StringBuffer( "ElementSpacing = " );
+				for ( int i = 0; i < 3; ++i )
+				{
+					sb.append( elementSpacing[ i ] );
+					if ( i < 2 )
+						sb.append( " " );
+				}
+				out.println( sb );
+			}
+			else
+			{
+				out.println( line );
+			}
 		}
 	}
 }
